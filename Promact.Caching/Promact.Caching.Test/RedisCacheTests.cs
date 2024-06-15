333c1308ca6d2094082faf5c9fcf5b41c927d505
Chintan Shah <chintanshah@promactinfo.com>
2024-06-15 15:34:28 +0530

    Enhance caching and CI/CD setup
    - Added Microsoft.Extensions.Caching and Promact.Core packages to Promact.Caching.Test.csproj.
    - Removed obsolete UnitTest1.cs file.
    - Enabled package generation in Promact.Caching.csproj.
    - Introduced a GitHub Actions workflow for CI/CD to NuGet.
    - Added RedisCacheTests for Redis distributed cache testing.

---
+++ Promact.Caching/Promact.Caching.Test/RedisCacheTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Promact.Core.Caching;
+
+
+namespace Promact.Caching.Test
+{
+    [TestClass]
+    public class RedisCacheTests
+    {
+        private IDistributedCache _distributedCache;
+        private ICachingService _cacheService;
+        private string[] _keys = new string[] { "TestKey1", "TestKey2", "TestKey3" };
+        [TestInitialize]
+        public void Setup()
+        {
+            _distributedCache = new RedisCache(new RedisCacheOptions
+            {
+                Configuration = "localhost:6379"
+            });
+
+            _cacheService = new DistributedCachingServices(_distributedCache);
+        }
+
+        [TestMethod]
+        public void AddDataSuccessTest()
+        {
+            var data = new TestData() { Name = "Test", Age = 20 };
+            _cacheService.Set("TestKey1", data);
+
+            var result = _cacheService.Get<TestData>("TestKey1");
+            Assert.IsNotNull(result);
+            Assert.AreEqual(data.Name, result.Name);
+            Assert.AreEqual(data.Age, result.Age);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _keys.ToList().ForEach(key => _cacheService.Remove(key));
+        }
+    }
+
+    internal class TestData
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
